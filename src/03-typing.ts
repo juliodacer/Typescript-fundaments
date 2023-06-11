5e9dd3398741b10dd0b21bf974e2432ed1c39968
Julio Davila <juliodacer@gmail.com>
2023-06-11 09:47:28 -0500

    03 typing

---
+++ src/03-typing.ts
@@ -0,0 +1,9 @@
+let myProductgName = 'Product 1'
+let myProductgPrice = 5
+
+myProductgName = '3'
+myProductgName.toLowerCase() // metodos propios de strings
+
+myProductgPrice.toFixed() // metodos propios de valores numericos
+
+const myProductStock = 1000
